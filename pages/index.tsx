32be9c954da279474060cc5329a5dc3ee5e464cb
Stefano Tambellini <s.tambellini@icloud.com>
2022-02-27 17:54:23 +0100

    fix: fouc

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,7 @@ import siteMetadata from "../data/siteMetadata";
 
 const Home: NextPage = () => {
   let [locale, setLocale] = useState("en");
+  let [mounted, setMounted] = useState(false);
 
   useEffect(function onFirstMount() {
     let browserLocale = window.navigator.language.split("-")[0];
@@ -19,93 +20,99 @@ const Home: NextPage = () => {
     }
 
     setLocale(browserLocale);
+
+    setMounted(true);
   }, []);
 
   return (
-    <div className="p-8 sm:p-12 md:p-16 lg:24">
-      <div>
-        <WebGL />
+    <>
+      {mounted && (
+        <div className="p-8 sm:p-12 md:p-16 lg:24">
+          <div>
+            <WebGL />
 
-        <div
-          className="relative z-30"
-          style={{
-            minHeight: "min-content",
-          }}
-        >
-          <div
-            className="antialiased"
-            style={{
-              textShadow: "0 2px 4px rgba(255,255,255,0.10)",
-            }}
-          >
-            <div className="md:max-w-4xl">
-              <PageTitle>
-                {messages.ciaoSono[locale]}{" "}
-                <span
-                  style={{
-                    backgroundImage: "url(./curcuma.jpeg)",
-                    backgroundColor: "rgb(255,180,4)",
-                    backgroundBlendMode: "saturation",
-                    backgroundRepeat: "no-repeat",
-                    backgroundSize: "cover",
-                    padding: "0.1em",
-                  }}
-                >
-                  Stefano
-                </span>
-              </PageTitle>
+            <div
+              className="relative z-30"
+              style={{
+                minHeight: "min-content",
+              }}
+            >
+              <div
+                className="antialiased"
+                style={{
+                  textShadow: "0 2px 4px rgba(255,255,255,0.10)",
+                }}
+              >
+                <div className="md:max-w-4xl">
+                  <PageTitle>
+                    {messages.ciaoSono[locale]}{" "}
+                    <span
+                      style={{
+                        backgroundImage: "url(./curcuma.jpeg)",
+                        backgroundColor: "rgb(255,180,4)",
+                        backgroundBlendMode: "saturation",
+                        backgroundRepeat: "no-repeat",
+                        backgroundSize: "cover",
+                        padding: "0.1em",
+                      }}
+                    >
+                      Stefano
+                    </span>
+                  </PageTitle>
 
-              <br />
-              <br />
+                  <br />
+                  <br />
 
-              <div className="text-2xl md:text-4xl leading-tight sm:leading-snug tracking-tight whitespace-pre-line">
-                <p>
-                  {messages.presentazione[locale]}
-                  {/* <br />
+                  <div className="text-2xl md:text-4xl leading-tight sm:leading-snug tracking-tight whitespace-pre-line">
+                    <p>
+                      {messages.presentazione[locale]}
+                      {/* <br />
                   Amo l'arte digitale, la pizza capricciosa e i videogiochi
                   indipendenti. */}
-                </p>
+                    </p>
 
-                {/* <p>
+                    {/* <p>
                   Sono sempre alla ricerca di progetti interessanti e
                   d'ispirazione!
                 </p>
 
                 <br /> */}
 
-                <p>
-                  <span>
-                    {messages.contattamiA[locale]}
-                    {": "}
-                  </span>
-                  <CustomLink href={`mailto:${siteMetadata.email}`}>
-                    {siteMetadata.email}
-                  </CustomLink>
-                </p>
-              </div>
+                    <p>
+                      <span>
+                        {messages.contattamiA[locale]}
+                        {": "}
+                      </span>
+                      <CustomLink href={`mailto:${siteMetadata.email}`}>
+                        {siteMetadata.email}
+                      </CustomLink>
+                    </p>
+                  </div>
 
-              <br />
-              <br />
+                  <br />
+                  <br />
 
-              <div className="text-xl md:text-3xl leading-tight sm:leading-snug tracking-tight whitespace-pre-line">
-                <p>
-                  <span className="mr-1">
-                    {messages.puoiAncheSeguirmiA[locale]}
-                    {": "}
-                  </span>
+                  <div className="text-xl md:text-3xl leading-tight sm:leading-snug tracking-tight whitespace-pre-line">
+                    <p>
+                      <span className="mr-1">
+                        {messages.puoiAncheSeguirmiA[locale]}
+                        {": "}
+                      </span>
 
-                  <Footer />
-                </p>
+                      <Footer />
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
 
-      <div className="z-50 fixed bottom-10 right-20">
-        <ThemeSwitcher />
-      </div>
-    </div>
+          <div className="z-50 fixed bottom-10 right-20">
+            <ThemeSwitcher />
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
