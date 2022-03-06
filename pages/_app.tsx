9d7806f2e134d390d8a6c6e28b36e61b1acef20f
Stefano Tambellini <s.tambellini@icloud.com>
2022-03-06 20:49:17 +0100

    opengraph

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,6 +17,25 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
+        <meta property="og:title" content="Stefano Tambellini" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://stefanotambellini.it" />
+        <meta
+          property="og:image"
+          content="https://stefanotambellini.it/og_image.jpg"
+        />
+        <meta
+          property="og:description"
+          content="Glad for now, grateful for ever"
+        />
+
+        <meta name="twitter:card" content="summary" />
+
+        <meta name="msapplication-TileColor" content="#da532c"></meta>
+        <meta name="theme-color" content="#ffffff"></meta>
+
+        <title>Stefano Tambellini</title>
+
         <link
           rel="apple-touch-icon"
           sizes="180x180"
@@ -36,11 +55,6 @@ export default function App({ Component, pageProps }: AppProps) {
         />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#da532c" />
         <link rel="manifest" href="/site.webmanifest" />
-
-        <meta name="msapplication-TileColor" content="#da532c"></meta>
-        <meta name="theme-color" content="#ffffff"></meta>
-
-        <title>Stefano Tambellini</title>
       </Head>
       <Analytics />
       <Component {...pageProps} />
